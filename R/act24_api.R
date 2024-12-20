388deefe1fdcc079b12d1e215322b8bff1b2e65c
Daniel Russ <druss@mail.nih.gov>
2024-12-20 13:54:34 -0500

    added Start/end date, mutliple studyIds

--- R/act24_api.R
+++ R/act24_api.R
@@ -1,4 +1,4 @@
-base_url = "https://act24.cancer.gov/act24/"
+base_url <- "https://act24.cancer.gov/act24"
 
 #' Call Act24 API
 #'
@@ -16,6 +16,8 @@ base_url = "https://act24.cancer.gov/act24/"
 #'  \strong{getDetailReportQC(studyId)} returns the detailed tibble of activities for a study
 #'
 #' @param studyId The studyId assigned by act24. Found in the getStudies results
+#' @param startDate The earliest recall date: format YYYY-MM-DD
+#' @param endDate The latest recall date: format YYYY-MM-DD
 #' @return a tibble that depends on the api called
 #' @name Act24_API_Wrapper
 #'
@@ -27,7 +29,7 @@ getStudies <- function(){
   if (is.null(.auth) ) {
     stop("not authenticated")
   }
-  api_url <- glue::glue("{base_url}studyList/")
+  api_url <- glue::glue("{base_url}/studyList/")
 
   make_act24_api_call(api_url)
 }
@@ -39,30 +41,67 @@ getParticantList <-function(studyId){
     stop("not authenticated")
   }
   if (!is.numeric(studyId)) stop("Invalid Study Id",studyId)
-  api_url <- glue::glue("{base_url}participantList/{studyId}/")
+  if (length(studyId)>1) stop("participantList can only have 1 study")
+  api_url <- glue::glue("{base_url}/participantList/{studyId}/")
 
   make_act24_api_call(api_url)
 }
 
 #' @rdname Act24_API_Wrapper
 #' @export
-getDetailReportQC <-function(studyId){
-  if (is.null(.auth)) {
-    stop("not authenticated")
-  }
-  if (!is.numeric(studyId)) stop("Invalid Study Id",studyId)
-  api_url <- glue::glue("{base_url}detailReportQC/{studyId}/")
+getSummaryReportQC <- function(studyId,startDate=NULL,endDate=NULL){
+  build_api_call("summaryReportQC",studyId,startDate,endDate) |>
+    make_act24_api_call()
+}
 
-  make_act24_api_call(api_url)
+#' @rdname Act24_API_Wrapper
+#' @export
+getDetailReportQC <-function(studyId,startDate=NULL,endDate=NULL){
+  build_api_call("detailReportQC",studyId,startDate,endDate) |>
+    make_act24_api_call()
+}
+
+#' @rdname Act24_API_Wrapper
+#' @export
+getDetailReport <-function(studyId,startDate=NULL,endDate=NULL){
+  build_api_call("detailReport",studyId,startDate,endDate) |>
+    make_act24_api_call()
 }
 
 
+build_api_call<-function(api,studyId,startDate=NULL,endDate=NULL){
+  if (!is_auth()) {
+    stop("not authenticated")
+  }
+  if (!is.numeric(studyId)) stop("Invalid Study Id",studyId)
+  if (length(studyId)>1){
+    studyId = paste(studyId,collapse = ",")
+  }
+  if (is.null(startDate) && !is.null(endDate)){
+    stop("cannot set endDate without setting startDate")
+  }
+
+
+  # reformat the start date/end date
+  dplyr::case_when(
+    !is.null(startDate) && !is.null(endDate) ~ paste0(base_url,"/",api,"/",studyId,"/",startDate,"/",endDate,"/"),
+    !is.null(startDate) ~ paste0(base_url,"/",api,"/",studyId,"/",startDate,"/"),
+    is.null(startDate) ~ paste0(base_url,"/",api,"/",studyId,"/"),
+  )
+}
+
 make_act24_api_call<-function(api_url){
+
   if (!is_auth()) stop("call act24_auth(userid) before making API calls")
   credentials <- get_credentials()
+  # TO DO: check response for:
+  # 401: incorrect username/id
+  # 422: Invalid study id. study id does not belong to you, general error
   httr2::request(api_url) |>
     httr2::req_auth_basic(credentials$api_user_id, password = credentials$act_pwd) |>
     httr2::req_perform() |>
     httr2::resp_body_json() |>
     dplyr::bind_rows()
 }
+
+
--- R/auth.R
+++ R/auth.R
@@ -2,24 +2,46 @@
 
 #' Provide API Linkage Id/password
 #'
-#' act24_auth sets the api linkage id (\strong{not the ACT24 research id}) and password.
-#' The password is cached until you close your R session
+#' act24_auth sets the api linkage id (\strong{not the ACT24 research id}) and
+#' password. The password is stored in a machine-specific manner based on the
+#' package keyring.  You can add .ignore_keyring to avoid storing the linked
+#' api password in the keychain. Even if you dont store the api password in the
+#' keychain, the id and password are cached in memory for the duration of the
+#' R session, unless you call act24_deauth.  The keyring can be cleared by
+#' calling act24_deauth with clear_keychain = TRUE
+#'
 #'
 #' act24_deauth removes the api linkage information.
 #'
 #' @param api_id The ACT24 API Linkage id
+#' @param .ignore_keyring If set to true, will not use the keyring to store
+#'  the api password across R sessions
+#' @param clear_keyring If set to true, will clear the id/password stored in
+#'  the keyring
 #'
 #' @export
 #'
-act24_auth <- function(api_id){
+act24_auth <- function(api_id,.ignore_keyring=FALSE){
   .auth$api_user_id <- api_id
-  .auth$act_pwd <- getPass::getPass("Enter Act24 API password: ")
+  # check if the users password is in the keyring
+  if ( api_id %in% keyring::key_list("act24")$username && !.ignore_keyring){
+    .auth$act_pwd <- keyring::key_get("act24",api_id)
+  } else{
+    .auth$act_pwd <- getPass::getPass("Enter Act24 API password: ")
+    if (!.ignore_keyring){
+      keyring::key_set_with_value("act24",api_id,.auth$act_pwd)
+    }
+  }
+  invisible(.auth)
 }
 
 #' @rdname act24_auth
 #' @export
-act24_deauth <- function(){
-  rm("api_user_id","act_pwd",envir = .auth)
+act24_deauth <- function(clear_keyring=FALSE){
+  if (clear_keyring){
+    keyring::key_delete("act24",.auth$api_user_id)
+  }
+  suppressWarnings(rm("api_user_id","act_pwd",envir = .auth))
 }
 
 get_credentials <- function(){
